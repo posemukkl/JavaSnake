874c9150e9b13fba6084e28b6135951311536ab9
posemukkl <posemukkl@gmail.com>
2015-09-25 14:17:02 +0200

    Initial Commit

---
+++ src/Apple.java
@@ -0,0 +1,13 @@
+public class Apple {
+	int posX;
+	int posY;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Snake.java
@@ -0,0 +1,19 @@
+
+public class Snake {
+	int length;
+	int[] body;
+	int posX;
+	int posY;
+	
+	public void setLength (int length){
+		this.length = length;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
