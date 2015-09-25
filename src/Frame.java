60aa44b830050e0c97a4d705264fcf3907f15b78
posemukkl <posemukkl@gmail.com>
2015-09-25 14:45:08 +0200

    1

--- src/Frame.java
+++ src/Frame.java
@@ -1,12 +1,13 @@
 import javax.swing.JFrame;
 public class Frame {
-
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MyKeyListener listener = new MyKeyListener();
+		
 	}
 
 }
---
+++ src/MyKeyListener.java
@@ -0,0 +1,33 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+public class MyKeyListener implements KeyListener {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
